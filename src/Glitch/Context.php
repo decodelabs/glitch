1451c680ad01d383f992a1fd41426a2861c4c00d
Tom Wright <tom@inflatablecookie.com>
2019-10-31 18:06:04 +0000

    Bumped version
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Glitch/Context.php
+++ src/Glitch/Context.php
@@ -33,7 +33,7 @@ class Context implements LoggerAwareInterface, FacadeTarget
     use FacadeTargetTrait;
 
     const FACADE = 'Glitch';
-    const VERSION = 'v0.15.1';
+    const VERSION = 'v0.15.2';
 
     protected $startTime;
     protected $runMode = 'development';
