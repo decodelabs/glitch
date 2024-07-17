1edd8cea18fe7fffe628be02210d4b575c5b2880
Tom Wright <tom@inflatablecookie.com>
2024-07-17 12:20:41 +0000

    Updated changelog
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Glitch/Context.php
+++ src/Glitch/Context.php
@@ -30,7 +30,7 @@ use Throwable;
 
 class Context implements LoggerAwareInterface
 {
-    public const VERSION = 'v0.18.15';
+    public const VERSION = 'v0.18.16';
 
     protected float $startTime;
     protected string $runMode = 'development';
